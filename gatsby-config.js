b49f10095c9fa8fcc81d80cce939d995c4396a47
sunquan1991 <10691963+sunquan1991@users.noreply.github.com>
2020-03-14 03:50:18 -0400

    Create navigation bar

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Qice Sun`,
+    description: `Qice Sun's porfolio site`,
+    author: `Qice Sun`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
---
+++ src/components/nav.js
@@ -0,0 +1,44 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+import styles from "./nav.module.css"
+
+const Nav = ({ siteTitle }) => (
+  <nav className={styles.wrapper}>
+    <div className={styles.main}>
+      <h1 className={styles.siteTitle}>
+        <Link className={styles.siteTitleText} to="/">{siteTitle}</Link>
+      </h1>
+      <div className={styles.works}>
+        <h3><Link to="/">Works</Link></h3>
+        <h5><Link to="/">Web</Link></h5>
+        <h5><Link to="/">Unity (AR & VR & Animation)</Link></h5>
+        <h5><Link to="/">Physical Computing</Link></h5>
+      </div>
+
+      <h3>
+        <Link to="/">About</Link>
+      </h3>
+
+      <h6 className={styles.contact}>
+        Email: sunquan.1991@yahoo.com
+        <br />
+        GitHub: <a href="https://github.com/sunquan1991" target="_blank">https://github.com/sunquan1991</a>
+        <br />
+        © 2020, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </h6>
+    </div>
+  </nav>
+)
+
+Nav.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Nav.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Nav
